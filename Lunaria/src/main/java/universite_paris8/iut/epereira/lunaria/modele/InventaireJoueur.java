c0d236d1f5eb91474c408946f34586e4f6cac53f
MCHEN <miamcooki@gmail.com>
2025-05-19 10:43:54 +0200

    inventaire joueur

--- Lunaria/src/main/java/universite_paris8/iut/epereira/lunaria/modele/InventaireJoueur.java
+++ Lunaria/src/main/java/universite_paris8/iut/epereira/lunaria/modele/InventaireJoueur.java
@@ -3,27 +3,44 @@ package universite_paris8.iut.epereira.lunaria.modele;
 import javafx.scene.layout.HBox;
 
 public class InventaireJoueur {
-    private Item[][] listeditem;
+    private Item[] listeditem;
 
     public InventaireJoueur(){
+        listeditem = new Item[9];
     }
 
-    public Item addItem(HBox invcase ){
 
-        switch (invcase.getId()){
-            case "case1":
-                System.out.println("case1");
-                break;
-            case "case2":
-                System.out.println("case2");
-                break;
-            case "case3":
-                System.out.println("case3");
-                break;
-            case "case4":
-                System.out.println("case4");
-                break;
+
+    //met un item dans l'inventaire, si l espace est occupé, on retourne l'item dans la position
+    public Item addItem(int pos, Item item){
+        Item retour = null;
+
+        if(listeditem[pos] != null){
+            retour = listeditem[pos];
         }
-        return null;
+        listeditem[pos] = item;
+        return retour;
     }
+
+    //enlève l'item a l'emplacement
+    public Item removeItem(int pos){
+        if(listeditem[pos] == null){
+            return null;
+        }
+        else{
+            return listeditem[pos];
+        }
+    }
+
+    //retourne la liste des item
+    public Item[] getListeditem() {
+        return listeditem;
+    }
+    //return un item dans l'inventaire
+    public Item getIteminList(int pos){
+        return listeditem[pos];
+    }
+
+
+
 }
