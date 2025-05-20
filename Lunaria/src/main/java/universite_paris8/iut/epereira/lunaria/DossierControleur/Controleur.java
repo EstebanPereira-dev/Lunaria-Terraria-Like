88a7b0287f807ef90f1a0fb77710792bb4314284
MCHEN <miamcooki@gmail.com>
2025-05-20 13:41:11 +0200

    ajout commentaire et inventaire fonctionelle sans image pour remplir les case

--- Lunaria/src/main/java/universite_paris8/iut/epereira/lunaria/DossierControleur/Controleur.java
+++ Lunaria/src/main/java/universite_paris8/iut/epereira/lunaria/DossierControleur/Controleur.java
@@ -26,32 +26,55 @@ import java.util.*;
 
 public class Controleur implements Initializable {
     @FXML
-    private GridPane inventaireGridPane;
+    private GridPane inventaireGridPane;  //gridPane qui contient les emplacement d'inventaire
+
     @FXML
-    private TilePane tilePaneId;
-    @FXML
+    private TilePane tilePaneId; //tilePane pour poesr nos bloc et les casser
+
+    @FXML //pouor afficher les item dans l'inventaire
+    private ImageView imageInv1,imageInv2,imageInv3,imageInv4,imageInv5,imageInv6,imageInv7,imageInv8,imageInv9;
+
+    @FXML //Texte Pause quand on arrete le jeux
     private TextArea pauseID;
-    @FXML
+
+    @FXML //le Pane qui contient tout
     private Pane tabJeu;
-    @FXML
+
+    @FXML //Afficher le fond
     private ImageView background;
 
+    //contient l'item dans la souris
     private Item tempItemSouris;
 
+    //lier acteur avec leur sprites
     private final Map<Acteur, Circle> sprites = new HashMap<>();
 
+    //Environement modele
     private Environement env;
+
+    //Controler la map
     private GestionnaireMap gestionMap;
 
+    //boolean popur savoir si l'inventaire est ouvert ou pas.
+    private boolean inventaireBooleanOvert = true;
+
+    //game loop
     private Timeline gameLoop;
 
-    @Override
+
+
+    @Override //initialization
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //Création de l'environement de la taille de l'écren
         env = new Environement(ConfigurationJeu.WIDTH_SCREEN, ConfigurationJeu.HEIGHT_SCREEN);
 
+        //initialiser ce que contient la souris a null
         tempItemSouris = null;
 
+        //appelle de la methode serInvVisible
+        setInvVisible(false);
 
+        //régler les taille des different Pane
         tabJeu.setPrefWidth(ConfigurationJeu.WIDTH_SCREEN);
         tabJeu.setPrefHeight(ConfigurationJeu.HEIGHT_SCREEN);
 
@@ -61,14 +84,16 @@ public class Controleur implements Initializable {
         background.setFitWidth(ConfigurationJeu.WIDTH_SCREEN);
         background.setFitHeight(ConfigurationJeu.HEIGHT_SCREEN);
 
+        //initialisation du gestionaire de la map
         gestionMap = new GestionnaireMap(tilePaneId, env);
 
+        //ajouter tous les acteurs dans la vue
         ajouterActeurVue(env.getHero());
         for (Acteur a : env.getActeurs()) {
             ajouterActeurVue(a);
         }
 
-
+        //démarage du jeux
         configurerEvenements();
         creerBoucleDeJeu();
         Platform.runLater(() -> {
@@ -81,11 +106,23 @@ public class Controleur implements Initializable {
         });
     }
 
+
+    //faire apparaitre/disparaitre l'inventaire
+    private void setInvVisible(Boolean bool) {
+        inventaireGridPane.setVisible(bool);
+        inventaireGridPane.setDisable(!bool);
+        for(int i = 0;i<9;i++){
+            inventaireGridPane.getChildren().get(i).setVisible(bool);
+        }
+    }
+
+    //pour chaque entré de touche
     private void configurerEvenements() {
         tabJeu.setOnKeyPressed(this::gererTouchePressee);
         tabJeu.setOnKeyReleased(this::gererToucheRelachee);
     }
 
+    //gestionaire de l'inventaire
     @FXML
     public void inv(ActionEvent event) {
         Node source = (Node) event.getSource();
@@ -93,41 +130,54 @@ public class Controleur implements Initializable {
         Integer col = GridPane.getColumnIndex(source);
 
 
-        Item temp = env.getHero().getInv().getIteminList(row*3+col);
+        Item temp = env.getHero().getInv().getIteminList(row * 3 + col);
 
-        if( temp != null){
+        //vérifie si l'emplacement sur le quel on click est occupé
+        //si occupé, echangé ce que contient la souris avec le contenue de l'emplacement
+        //sinon, juste placer ce que contient la souris dans l'emplacement
+        if (temp != null) {
             Item switch2 = temp;
-            env.getHero().getInv().addItem(row*3+col,tempItemSouris);
+            env.getHero().getInv().addItem(row * 3 + col, tempItemSouris);
             tempItemSouris = switch2;
-        }
-        else{
-            env.getHero().getInv().addItem(row*3+col,tempItemSouris);
+        } else {
+            env.getHero().getInv().addItem(row * 3 + col, tempItemSouris);
         }
 
-        //inventaireGridPane.getChildren().get(row*3+col).setStyle("-fx-background-image: ");
+
+        //charger ce que contient la case par une image de l'item, vide si null
+
+        //imageInv1.setImage(new Image("/universite_paris8/iut/epereira/lunaria/DossierMap/Background.png"));
+
+        //((Node) event.getSource()).setStyle("-fx-background-image: url("+getClass().getResource("/universite_paris8/iut/epereira/lunaria/DossierMap/Background.png")+")");
+        //((Node) event.getSource()).setOpacity(1.0);
+
+        //inventaireGridPane.getChildren().get(row * 3 + col).setStyle();
+
 
     }
-    @FXML
+
+    @FXML//si la souris est cliquer attaquer
     public void clicSouris() {
         attaqueHero();
     }
 
-    @FXML
-    public void plusClicSouris(){
+    @FXML //rien
+    public void plusClicSouris() {
     }
 
+    //gere quand les touche sont appuyé, faire une action
     private void gererTouchePressee(KeyEvent event) {
         switch (event.getCode()) {
-            case SPACE, Z:
+            case SPACE, Z: //sauter
                 env.getHero().getActions().set(0, true);
                 break;
-            case Q:
+            case Q: //aller a gauche
                 env.getHero().getActions().set(3, true);
                 break;
-            case D:
+            case D: //aller a droite
                 env.getHero().getActions().set(2, true);
                 break;
-            case ESCAPE:
+            case ESCAPE: //mettre le jeux en pause
                 env.getHero().getActions().set(5, !env.getHero().getActions().get(5));
                 if (env.getHero().getActions().get(5)) {
                     pauseID.setVisible(true);
@@ -137,23 +187,25 @@ public class Controleur implements Initializable {
                     pauseID.setVisible(false);
                 }
                 break;
-            case I:
+            case I: //ouvrire ou fermer l'inventaire
                 if (env.getHero().getActions().get(5)) {
 
                 } else {
                     env.getHero().getActions().set(4, !env.getHero().getActions().get(4));
-                    if (env.getHero().getActions().get(4)) {
-                        inventaireGridPane.setVisible(true);
-                        inventaireGridPane.setDisable(true);
-                    } else {
-                        inventaireGridPane.setVisible(false);
-                        inventaireGridPane.setDisable(true);
+                    if(inventaireBooleanOvert){
+                        setInvVisible(true);
+                        inventaireBooleanOvert = !inventaireBooleanOvert;
                     }
+                    else{
+                        setInvVisible(false);
+                        inventaireBooleanOvert = !inventaireBooleanOvert;
+                    }
+
                 }
                 break;
         }
     }
-
+    //gere quand la touche est relacher
     private void gererToucheRelachee(KeyEvent event) {
         switch (event.getCode()) {
             case SPACE, Z:
@@ -168,6 +220,7 @@ public class Controleur implements Initializable {
         }
     }
 
+    //création de la boucle de jeux
     private void creerBoucleDeJeu() {
         gameLoop = new Timeline(
                 new KeyFrame(Duration.millis(15), e -> miseAJourJeu())
@@ -175,6 +228,7 @@ public class Controleur implements Initializable {
         gameLoop.setCycleCount(Animation.INDEFINITE);
     }
 
+    //gere ce qui se passe toute les tick
     private void miseAJourJeu() {
         List<Acteur> acteursCopie = new ArrayList<>(env.getActeurs());
 
@@ -192,7 +246,9 @@ public class Controleur implements Initializable {
 
 
     }
-    public void attaqueHero(){
+
+    //à commenter
+    public void attaqueHero() {
         if (!env.getHero().attackOnCooldown) {
             env.getHero().getActions().set(6, true);
             env.getHero().attaque();
@@ -202,7 +258,6 @@ public class Controleur implements Initializable {
             });
 
             env.getHero().attackOnCooldown = true;
-
             Timeline attackCooldownTimer = new Timeline(
                     new KeyFrame(Duration.seconds(3), e -> {
                         env.getHero().attackOnCooldown = false;
@@ -216,6 +271,8 @@ public class Controleur implements Initializable {
             System.out.println("COOLDOWN");
         }
     }
+
+    //à commenter
     public void attaqueEnnemis() {
         for (Acteur acteur : env.getActeurs()) {
             if (acteur instanceof Ennemi && !acteur.attackOnCooldown) {
@@ -235,6 +292,8 @@ public class Controleur implements Initializable {
             }
         }
     }
+
+    //supprime tout les sprites mort
     public void supprimerActeurVue(Acteur acteur) {
         Circle sprite = sprites.get(acteur);
         if (sprite != null) {
@@ -243,6 +302,7 @@ public class Controleur implements Initializable {
         }
     }
 
+    //crée un sprite
     public Circle creerSprite(Acteur a) {
         Circle r = new Circle(8);
         if (a instanceof Hero) {
@@ -267,6 +327,7 @@ public class Controleur implements Initializable {
         gameLoop.stop();
     }
 
+    //ajoute un acteur dans la vue
     public void ajouterActeurVue(Acteur acteur) {
         Circle sprite = creerSprite(acteur);
         sprites.put(acteur, sprite);
