d748b1414a731e02520248662c9076ff50288b5c
MCHEN <miamcooki@gmail.com>
2025-05-27 12:12:56 +0200

    crafting system

--- Lunaria/src/main/java/universite_paris8/iut/epereira/lunaria/modele/Craft.java
+++ Lunaria/src/main/java/universite_paris8/iut/epereira/lunaria/modele/Craft.java
@@ -1,41 +1,82 @@
-//package universite_paris8.iut.epereira.lunaria.modele;
-//
-//import java.util.ArrayList;
-//
-//public class Craft {
-//
-//    private Environement env;
-//    private ArrayList<Item> recette;
-//    private Item resultat;
-//
-//    public Craft(Environement env, Item resultat) {
-//        this.env = env;
-//        this.resultat = resultat;
-//        recette = new ArrayList<>();
-//    }
-//
-//
-//    public void addItem(Item item) {
-//        recette.add(item);
-//    }
-//
-//
-//    public boolean craftable() {
-//        for (Item itemRecette : recette) {
-//            int compteur = 0;
-//            for (Item item : env.getHero().getInv().getListeditem()) {
-//                if (item.getNom().equals(itemRecette.getNom())) {
-//                    compteur += item.getQuantite();
-//                }
-//            }
-//            if (compteur < itemRecette.getQuantite()) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
-//
-//
+package universite_paris8.iut.epereira.lunaria.modele;
+
+import java.util.ArrayList;
+
+public class Craft {
+
+    private Environement env;
+    private ArrayList<Item> recette;
+    private ArrayList<Integer> quantite;
+    private Item resultat;
+
+    public Craft(Environement env, Item resultat) {
+        this.env = env;
+        this.resultat = resultat;
+        recette = new ArrayList<>();
+        quantite = new ArrayList<>();
+    }
+
+
+    public void addItem(Item item, int quantite) {
+        recette.add(item);
+        this.quantite.add(quantite);
+    }
+
+
+    //regarde si le hero as tout les ingrédiant pour craft l'item
+    public boolean craftable(){
+
+        for(int j = 0; j< recette.size();j++){
+            int quantiteNecessaire = quantite.get(j);
+
+            for(int i = 0; i < env.getHero().getInv().getTaille(); i++){
+                if(env.getHero().getInv().getListeditem()[i].getId() == recette.get(j).getId()){
+                    quantiteNecessaire -= env.getHero().getInv().getQuantite()[i];
+                }
+            }
+            //si apres avoir parcourus tout l'inventaire, il manque des ressource, alors return false
+            if(quantiteNecessaire > 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    public Item Crafting(){
+        if(craftable()){
+            //parcours de la liste d item de la recette
+            for(int j = 0; j < recette.size(); j++){
+                int quantiteAPrendre = quantite.get(j);
+                //parcours de la liste d item dans l inventaire
+                for (int i = 0;i < env.getHero().getInv().getTaille() && quantiteAPrendre > 0;i++){
+
+                    //recherche la meme id de ressource dans l inventaire
+                    if(env.getHero().getInv().getListeditem()[i].getId() == recette.get(j).getId()){
+                        //si la quantité dans l inventaire est supérieur a la quantite demander
+                        //alors enlever la quantite a l inventaire
+                        if(quantiteAPrendre < env.getHero().getInv().getQuantite()[i]){
+                            env.getHero().getInv().getQuantite()[i] -= quantiteAPrendre;
+                            quantiteAPrendre = 0;
+                        }
+                        //si la quandtite est moindre ou égal
+                        //alors supprimer de l'inventaire,
+                        //réduit la quantite necessaire apres avoir 'pris' ce qu'il y as dans l'inventaire
+                        // et continuer la recherche
+                        if(env.getHero().getInv().getQuantite()[i] <= quantiteAPrendre){
+                            quantiteAPrendre -= env.getHero().getInv().getQuantite()[i];
+                            env.getHero().getInv().getQuantite()[i] = 0;
+                            env.getHero().getInv().getListeditem()[i] = null;
+                        }
+                    }
+                }
+            }
+        }
+        return resultat;
+    }
+
+
 //    public Item crafting() {
 //        if (craftable()) { //si il a toute les ressource
 //            for (Item itemrec : recette) { //pour chaque item dans la recette
@@ -57,6 +98,6 @@
 //        }
 //        return resultat;
 //    }
-//
-//
-//}
+
+
+}
